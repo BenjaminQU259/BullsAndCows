90ebad949f5b7d20893e66c58b23c1a364368502
Minghao QU <MQu2@slb.com>
2022-10-28 14:48:18 +0800

    feat: return 2A0B when guess given guess digits having only 2 digits same position as secret

--- BullsAndCows/BullsAndCowsGame.cs
+++ BullsAndCows/BullsAndCowsGame.cs
@@ -5,16 +5,30 @@ namespace BullsAndCows
   public class BullsAndCowsGame
   {
     private readonly SecretGenerator secretGenerator;
+    private string secret;
+
     public BullsAndCowsGame(SecretGenerator secretGenerator)
     {
       this.secretGenerator = secretGenerator;
+      secret = secretGenerator.GenerateSecret();
     }
 
     public bool CanContinue => true;
 
     public string Guess(string guess)
     {
-      return "4A0B";
+      var guessDigits = guess.Split(" ");
+      var secretDigits = secret.Split(" ");
+      var countBulls = 0;
+      for (var index = 0; index < secretDigits.Length; index++)
+      {
+        if (guessDigits[index] == secretDigits[index])
+        {
+          countBulls++;
+        }
+      }
+
+      return $"{countBulls}A0B";
     }
   }
 }
--- BullsAndCowsTest/BullsAndCowsGameTest.cs
+++ BullsAndCowsTest/BullsAndCowsGameTest.cs
@@ -27,5 +27,18 @@ namespace BullsAndCowsTest
       //then
       Assert.Equal("4A0B", guessResult);
     }
+
+    [Fact]
+    public void Should_return_2A0B_when_guess_given_guess_having_only_2_digits_same_position_as_secret()
+    {
+      // given
+      var mockSecretGenerator = new Mock<SecretGenerator>();
+      mockSecretGenerator.Setup(_ => _.GenerateSecret()).Returns("1 2 3 4");
+      var game = new BullsAndCowsGame(mockSecretGenerator.Object);
+      //when
+      var guessResult = game.Guess("1 2 5 6");
+      //then
+      Assert.Equal("2A0B", guessResult);
+    }
   }
 }
